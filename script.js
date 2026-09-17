af63755e14e0fa1b527852b9513d8adcaec0893d
Ulrich Bouwer <ulla135532@gmail.com>
2026-09-17 11:04:33 +0200

    Add dynamic grid resizing

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 const container = document.querySelector("#container");
 
-for (let i = 0; i < 256; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
+const resizeBtn =document.querySelector("#resizeBtn");
 
-  square.addEventListener("mouseenter", () => {
-    square.style.backgroundColor = "black";
-  });
-  
-  container.appendChild(square);
+function createGrid(size) {
+  const squareSize = 640 / size;
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
+
+    square.addEventListener("mouseenter", () => {
+      square.style.backgroundColor = "black";
+    });
+
+    container.appendChild(square);
+  }
 }
+
+createGrid(16);
+
+resizeBtn.addEventListener("click", () => {
+  const newSize = prompt("How many squares per side?");
+
+  if (newSize < 1 || newSize > 100) {
+    return;
+  }
+
+  container.innerHTML = "";
+
+  createGrid(newSize) ;
+    
+});
